9c18f6dfba674fac2779dc3166589a0f2b07535a
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-02-19 14:13:56 +0100

    docs: fix minor issues

--- create-project.sh
+++
@@ -1,4 +0,0 @@
-mvn io.quarkus.platform:quarkus-maven-plugin:3.7.3:create \
-    -DprojectGroupId=at.htl.minikube \
-    -DprojectArtifactId=minikube-demo \
-    -Dextensions='resteasy-reactive, smallrye-health'
