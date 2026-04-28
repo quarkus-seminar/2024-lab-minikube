16d116d462ef5cb3d13402d4c71aa7a47101aae4
htl-leonding <t.stuetz@htl-leonding.ac.at>
2026-04-28 16:38:09 +0200

    docs: bump quarkus to 3.34.6

--- asciidocs/docs/create-project.sh
+++ asciidocs/docs/create-project.sh
@@ -1,4 +1,4 @@
-mvn io.quarkus.platform:quarkus-maven-plugin:3.7.3:create \
+mvn io.quarkus.platform:quarkus-maven-plugin:3.34.6:create \
     -DprojectGroupId=at.htl.minikube \
     -DprojectArtifactId=minikube-demo \
     -Dextensions='resteasy-reactive, smallrye-health'
